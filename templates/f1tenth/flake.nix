8b4ef4115982cb4242c9450bf375d0b00732796d
bitSheriff <root@bitsheriff.dev>
2026-05-07 20:11:07 +0200

    use rosPackages globally

--- templates/f1tenth/flake.nix
+++ templates/f1tenth/flake.nix
@@ -5,6 +5,7 @@
     nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay/master";
     nixpkgs.follows = "nix-ros-overlay/nixpkgs";
     nixgl.url = "github:nix-community/nixGL";
+    nixgl.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
@@ -20,9 +21,139 @@
         "aarch64-linux"
       ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgs = import nixpkgs {
+        overlays = [ nix-ros-overlay.overlays.default ];
+      };
+
+      sharedRosPackages =
+        ros: with ros; [
+          ackermann-msgs
+          diagnostic-aggregator
+          geometry-msgs
+          joint-state-publisher
+          nav-msgs
+          nav2-lifecycle-manager
+          nav2-map-server
+          plotjuggler-ros
+          rosidl-default-generators
+          rosidl-default-runtime
+          sensor-msgs
+          teleop-twist-keyboard
+          tf2-geometry-msgs
+          tf2-ros
+          xacro
+          nav2-mppi-controller # Model Predictive Controller
+          navigation2
+          nav2-controller
+        ];
+
+      sharedRosPythonDependencies = with pkgs; [
+        # Python dependencies
+        python3
+        python3Packages.numpy
+        python3Packages.transforms3d
+        python3Packages.scikit-image
+        python3Packages.pip
+        python3Packages.numpy
+        python3Packages.scikit-image
+        python3Packages.transforms3d
+
+      ];
 
     in
     {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ nix-ros-overlay.overlays.default ];
+          };
+          ros = pkgs.rosPackages.jazzy;
+
+        in
+        {
+          clean = pkgs.stdenv.mkDerivation {
+            name = "Clean";
+            buildPhase = ''
+              echo "Deleting directories"
+              rm -rf install/
+              rm -rf log/
+            '';
+          };
+
+          default = pkgs.stdenv.mkDerivation {
+            name = "f1tenth-build";
+            src = ./.;
+
+            dontUseCmakeConfigure = true;
+
+            nativeBuildInputs = [
+              ros.ros-environment
+              ros.ament-cmake
+              ros.ament-cmake-ros
+              ros.ament-lint-auto
+              pkgs.colcon
+              pkgs.python3
+              pkgs.python3Packages.setuptools
+            ];
+
+            propagatedBuildInputs = [
+            ]
+            ++ (sharedRosPackages ros)
+            ++ (sharedRosPythonDependencies);
+
+            buildPhase = ''
+              export COLCON_EXTENSION_BLACKLIST=colcon_ros.prefix_path.ament
+              export PYTHONPATH="${pkgs.python3Packages.setuptools}/${pkgs.python3.sitePackages}:$PYTHONPATH"
+
+              colcon build --install-base $out
+              colcon build --install-base $out --symlink-install --packages-select f1tenth_gym
+              colcon build --install-base $out --symlink-install --packages-select f1tenth_gym_ros
+            '';
+
+            installPhase = ''
+              mkdir -p $out/bin
+
+              cp -r $src/install $out/
+              cp -r $src/launch $out/
+              cp -r $src/config $out/
+            '';
+          };
+        }
+      );
+
+      #### APPS ####
+      apps = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          lib = nixpkgs.lib;
+          mkSimApp = launchFile: {
+            type = "app";
+            program = lib.getExe (
+              pkgs.writeShellApplication {
+                name = "run-${launchFile}";
+                text = ''
+                  echo "Starting ROS 2 simulation for ${launchFile}..."
+                  set +u
+                  # shellcheck disable=SC1091
+                  source install/local_setup.bash
+                  set -u
+                  ros2 launch launch/${launchFile}
+                '';
+              }
+            );
+          };
+
+        in
+        {
+          sim = mkSimApp "sim.py";
+          pure_pursuit = mkSimApp "pure_pursuit_sim.py";
+        }
+      );
+
+      #### DEV SHELL ####
       devShells = forAllSystems (
         system:
         let
@@ -31,14 +162,15 @@
             overlays = [ nix-ros-overlay.overlays.default ];
           };
           rosShell = nix-ros-overlay.devShells.${system}.example-ros2-desktop-jazzy;
+          ros = pkgs.rosPackages.jazzy;
 
           # Explicitly grab the nixGL wrapper for your system
-          nixGLDefault = nixgl.packages.${system}.nixGLDefault;
+          nixGLDefault = nixgl.packages.${system}.nixGLIntel;
 
           build_colcon = pkgs.writeShellScriptBin "build_colcon" ''
             colcon build
             colcon build --symlink-install --packages-select f1tenth_gym
-            source install/local_setup.bash
+            colcon build --symlink-install --packages-select f1tenth_gym_ros
           '';
 
           helper-script = pkgs.writeShellApplication {
@@ -53,7 +185,6 @@
               if [ "$CHOSEN" = "print tf tree" ]; then
                   gum style --foreground 212 "Printing the tf tree to a pdf file"
                   ros2 run tf2_tools view_frames
-
 
               elif [ "$CHOSEN" = "build" ]; then
                   gum style --foreground 57 "Building the application"
@@ -71,48 +202,50 @@
             '';
           };
 
+          rviz2_wrapped = pkgs.writeShellScriptBin "rviz2" ''
+            # Force EGL to fix FBConfig mismatch in Jazzy (Qt6)
+            export QT_XCB_GL_INTEGRATION=xcb_egl
+            exec ${nixGLDefault}/bin/nixGLIntel ${ros.rviz2}/bin/rviz2 "$@"
+          '';
+
+          rqt_wrapped = pkgs.writeShellScriptBin "rqt_reconfigure" ''
+            exec ${nixGLDefault}/bin/nixGLIntel ${ros.rqt-reconfigure}/bin/rqt_reconfigure "$@"
+          '';
         in
         {
           default = pkgs.mkShell {
             name = "f1tenth-jazzy-shell";
-
             inputsFrom = [ rosShell ];
 
-            packages = with pkgs; [
-              # Hardware Acceleration
-              nixGLDefault
+            packages =
+              with pkgs;
+              [
+                # Hardware Acceleration
+                nixGLDefault
+                glxinfo
 
-              # ROS dependencies
-              rosPackages.jazzy.ackermann-msgs
-              rosPackages.jazzy.diagnostic-aggregator
-              rosPackages.jazzy.plotjuggler-ros
-              rosPackages.jazzy.nav2-lifecycle-manager
-              rosPackages.jazzy.nav2-map-server
-              rosPackages.jazzy.tf2-ros
-              rosPackages.jazzy.xacro
-              rosPackages.jazzy.joint-state-publisher
-              rosPackages.jazzy.teleop-twist-keyboard
+                # Development tools
+                bashInteractive
+                just
+                git
+                gum
+                tmux
+                lazygit
+                rsync
+                python3
 
-              # Development tools
-              just
-              git
-              gum
-              tmux
-              lazygit
-              python3
-              python3Packages.pip
-              python3Packages.numpy
-              python3Packages.scikit-image
-              python3Packages.transforms3d
+                # Scripts
+                build_colcon
+                helper-script
+                rviz2_wrapped
+                rqt_wrapped
+              ]
+              ++ (sharedRosPackages ros)
+              ++ (sharedRosPythonDependencies);
 
-              # Scripts
-              build_colcon
-              helper-script
-
-            ];
+            ROS_DOMAIN_ID = 69;
 
             shellHook = ''
-              export ROS_DOMAIN_ID=0
               export COLCON_EXTENSION_BLACKLIST=colcon_ros.prefix_path.ament
 
               # --- Local Python Virtual Environment ---
@@ -140,11 +273,16 @@
               # ----------------------------------------
 
               # --- Graphics Fix (via nixGL) ---
-              # Note: requires 'nix develop --impure'
               export QT_QPA_PLATFORM=xcb
-              # The binary is usually named 'nixGL' inside the nixGLDefault package
-              alias rviz2='${nixGLDefault}/bin/nixGL rviz2'
-              alias rviz='${nixGLDefault}/bin/nixGL rviz2'
+              export QT_XCB_GL_INTEGRATION=xcb_egl
+              export Ogre_GL_Config=GLX
+
+              # Set up nixGL and Mesa drivers
+              export LIBGL_DRIVERS_PATH="${pkgs.mesa}/lib/dri"
+              export LD_LIBRARY_PATH="${nixGLDefault}/lib:${pkgs.mesa}/lib:$LD_LIBRARY_PATH"
+
+              # Add wrapped GUI tools to PATH
+              export PATH="${rviz2_wrapped}/bin:${rqt_wrapped}/bin:$PATH"
 
               # --- Setup Consistency Symlinks ---
               mkdir -p src
@@ -162,7 +300,13 @@
                   rm -f "$target"
                   sed "s|/arc2026|$PWD|g" src/f1tenth_gym_ros/config/sim.yaml > "$target"
                 fi
-                command ros2 "$@"
+                
+                # Wrap GUI-heavy commands with nixGL to ensure rviz2/rqt work
+                if [[ "$1" == "launch" || "$1" == "run" ]]; then
+                  ${nixGLDefault}/bin/nixGLIntel ros2 "$@"
+                else
+                  command ros2 "$@"
+                fi
               }
 
               echo "--- F1Tenth ROS 2 Jazzy (Nix) ---"
